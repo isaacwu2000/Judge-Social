e9976ea4a6169c6a8916ff94108e1323de4d0244
Isaac Wu <isaacwu2000@gmail.com>
2025-03-09 17:46:03 -0400

    s

--- model.py
+++ model.py
@@ -28,7 +28,12 @@ def conversation(conversation_text):
     user = f"Give a CONCISE response to the last message in this text conversation. Don't use acronyms. Be human-like and somewhat casual. conversationMessages = '''{conversation_text}'''"
     return gemini(system, user)
 
-async def judgement():
-    pass
+def judgement(conversation):
+    # Reading the extensive persona file
+    with open('/prompts/judgement.txt', 'r') as judgement_guidelines_file:
+        judement_guidelines = judgement_guidelines_file.read()
+    # Have Gemini respond as though it has the persona
+    user = f"Evaluate and roast the User in this conversation based on your conversation judement and roasting guidelines '''{conversation}'''"
+    return gemini(judement_guidelines, user)
 
-print(conversation("User: How are you?"))
+print(judgement('AI: "Hey, long time no see! How have you been?" User: "Busy." AI: "Oh, I get that. What’s been keeping you busy?" User: "Work." AI: "Anything exciting happening?" User: "Nope."'))
